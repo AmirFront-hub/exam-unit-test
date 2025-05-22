ddfa756699492e67c7d7f20a00402111498a9442
Amir <amiralziyadi@gmail.com>
2025-05-22 18:33:30 +0200

    Remove commented instructions and unnecessary comments from cart.js for cleaner code - REFACTOR

--- src/cart.js
+++ src/cart.js
@@ -1,27 +1,7 @@
-/*
-Din uppgift:
-- skriv testfall för alla funktionerna nedan i cart.test.js (RED)
-- skriv kod här för att implementera funktionerna (GREEN)
-
-Tips:
-- börja med att identifiera VAD som ska testas.
-- om du testar t.ex. removeFromCart får du använda addToCart i början av testet. Den kommer nämligen ha sina egna tester
-
-*/
-// function getItem(index)
-// function getTotalCartValue()
-// function editCart(itemId, newValues)
-// -------------------------------------------------- //
-
 import { isCartItem, isProduct } from "./validation.js"
 
 let cart = []
 let idCounter = 2002
-// -------------------------------------------------- //
-
-
-// Din kod börjar här
-// Du får en funktion att börja med
 
 function clearCart(){
 		cart.length = 0;
