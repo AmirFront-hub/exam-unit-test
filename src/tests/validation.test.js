3afa0215ba139b363f4019a32886a589ebfca1b0
Amir <amiralziyadi@gmail.com>
2025-05-22 18:12:49 +0200

    Refactor validation tests for clarity and organization; remove unnecessary comments - GREEN

--- src/tests/validation.test.js
+++ src/tests/validation.test.js
@@ -12,28 +12,31 @@ const exampleCartObject = {
 	item: exampleProduct
 }
 
-// Group tests using "describe"
 describe('Validation', () => {
 
-	// ---------------------------------------------
-	// Följande testfall ska du implementera. Det är tillåtet att använda Joi. Gör i så fall ett schema för varje sorts objekt du vill kunna validera. Du får även ändra texten och du t.ex. vill skriva på svenska i stället för engelska.
-	// (Ta bort dessa kommentarer när du är klar)
-
-	// 1. it returns true for a valid cart object
 	test('it returns true for a valid cart object', () => {
-		
+    const result = isCartItem(exampleCartObject);
+    expect(result).toBe(true);
 	});
-	// 2. it returns false for invalid cart objects
-	test('it returns false for invalid cart objects', () => {
 
+	// Osäker ifall detta stämmer
+	
+	test('it returns false for invalid cart objects', () => {
+    const invalidCartObject = { id: 2002, amount: 1 };
+    const result = isCartItem(invalidCartObject);
+    expect(result).toBe(false);
 	});
-	// 3. it returns true for a valid product
+	
 	test('it returns true for a valid product', () => {
     const result = isProduct(exampleProduct);
     expect(result).toBe(true);
 	});
-	// 4. it returns false for invalid cart objects
-	test('it returns false for invalid cart objects', () => {
 
+	// Osäker ifall detta stämmer
+	
+	test('it returns false for an invalid product', () => {
+    const invalidProduct = { id: 1002, price: 100 };
+    const result = isProduct(invalidProduct);
+    expect(result).toBe(false);
 	});
 })
